51534a6597ea336501308b7052227e9663ce20c1
Ahmed Shalabi <Ashalabi@uwaterloo.ca>
2017-05-24 01:02:40 -0400

    Update Vector.py

--- Vector.py
+++ Vector.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon May 22 00:00:57 2017
-
 @author: Ahmed Shalabi
 """
 
-from math import sqrt , acos 
+from math import sqrt , acos , cos
 
 class Vector(object):
     def __init__(self, Input = []):
@@ -17,48 +15,55 @@ class Vector(object):
     
     def __add__(self,other):
         data = []
-        for i in len(range(self.data)):
+        i = 0
+        while (i < len(self.data)):
             data.append(self.data[i] + other.data[i])
+            i += 1
         return data
         
-    def __subt__(self,other):
+    def __sub__(self,other):
         data = []
-        for i in len(range(self.data)):
-            data.append(self.data[i] - other.data)
+        i=0
+        while(i<len(self.data)):
+            data.append(self.data[i] - other.data[i])
+            i += 1
         return data
     
-    def __mult__(self, v ):
+    def __mul__(self, v):
         data =[]
-        for i in len(range(self.data)):
+        i=0
+        while(i<len(self.data)):
             data.append( v * self.data[i] )
+            i += 1
         return data
     
-    def __dot__(self,other):
+    def dot(self,other):
         dot_product = 0
-        for i in len(range(self.data)):
-            dot_product += (self.data[i] * self.data[i])
+        i=0
+        while(i<len(self.data)):
+            dot_product += (self.data[i] * other.data[i])
+            i+=1
         return dot_product
     
-    def __norm__(self):
-        dot_product = Vector.dot (self,self)
+    def norm(self):
+        dot_product = self.dot(self)
         norm = sqrt(dot_product)
         return norm
     
-    def __unit__(self):
-        norm = Vector.norm(self)
-        unit_vec = Vector.__mult__(self , 1.0 / norm )
+    def unit(self):
+        norm = self.norm()
+        unit_vec = self*(1.0 / norm )
         return unit_vec
     
-    def __angle__(self,other):
-        dot_product = Vector.dot(self,other)
-        norm_A = Vector.norm(self)
-        norm_B = Vector.norm(other)
-        theta = acos(dot_product/norm_A*norm_B)
+    def angle(self,other):
+        dot_product = self.dot(other)
+        print(dot_product)
+        norm_A = self.norm()
+        norm_B = other.norm()
+        temp = dot_product/(norm_A*norm_B)
+        if temp>1.0:
+            temp = 1.0
+            
+        theta = acos(temp)
         return theta
-        
-V1 = Vector([0,0,0])
-V2 = Vector([1,1,1])
-print(V1+V2)
-        
-        
         
