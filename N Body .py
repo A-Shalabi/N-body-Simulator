3a5b9b0101077d6694786f8111e7fcb7b714d46f
Ahmed Shalabi <Ashalabi@uwaterloo.ca>
2017-11-23 22:55:09 -0500

    Update N Body .py

--- N Body .py
+++ N Body .py
@@ -1,44 +1,25 @@
-From Vector Import Vector
-
-class Body(Vector):
-    def __init__(self , mass , displacement=Vector() , velocity = Vector() , acceleration = Vector):
-        self.mass = mass
-        self.displacement = displacement
-        self.velocity = velocity
-        self.acceleration = acceleration
-        self.total_displacement = []
-        self.total_velocity = []
-        self.total_acceleration = []
-    def update_Acceleration(self,other):
-        G = 6.674e11
-        displacement = Vector()
-        self.acceleration = ( G * other.mass) * (1.0/Vector.mag(other.displacement - self.displacement)**3) * (other.displacement - self.displacement) 
-        return self.acceleration
-        
-    def update_velocity(self,t):
-        self.velocity = self.velocity + (self.acceleration * t)
-        return self.velocity
-    
-    def update_displacement(self,t):
-        self.displacement = self.position + (self.velocity * t )
-        return self.displacement
-        
-    def update_total_displacement(self,t):
-        self.total_displacement.append(Body.update_displacement(self,t))
-        return self.total_displacement
-    
-    def update_total_velocity(self,t):
-        self.total_velocity.append(Body.update_velocity(self,t))
-        return self.total_velocity
-    
-    def update_total_acceleration(self,other):
-        self.total_acceleration.append(Body.update_Acceleration(self,other))
-        return self.total_acceleration
+from Vector import Vector
+from N Body import N Body
+"""
+     
+    for body in Bodies:
+        for body in Bodies:
+            integration(self,other)
+            update_displacement(self)
+            update_total_displacement(self)"""
 
 def integration(body1, body2):
-        #define some time stamp
-        for #time stamp loop here#
-        
+        #define some time step
+        for i in range(self.N):
+            
+			p = self.particles[i]
+			displacement.append(p)
+			acceleration = []
+			for j in range(i) + range(i+1, self.N):
+				p2 = self.particles[j]
+				# Gravitational forces
+				update_total_acceleration()
+
         for t in tpoints:
             Body.append()
             k1 = h*f(r, t)
@@ -46,8 +27,19 @@ def integration(body1, body2):
             k3 = h*f(r + 0.5*k2,t+0.5*h)
             k4 = h*f(r + k3,t+h)
             r += (k1+2*k2+2*k3+k4)/6.0
-        
-def calculate(Bodies):
+
+def Gravitational_effect(self, p1, p2):
+    	dx = p1.x - p2.x
+    	dy = p1.y - p2.y
+    	r = p1.dist(p2)
+    	f = - p1.m * p2.m / (r**2 + self.a**2)
+    	return [f*dx, f*dy]
+    
+def take_step(self):
+	for i in range(self.N):
+		update_total_acceleration(self).updates
+
+  def calculate(Bodies):
     i=0
     while(i<len(Bodies)):
         j=0
@@ -56,10 +48,3 @@ def calculate(Bodies):
                 integration(Bodies[i], Bodies[j])
             j+=1
         i+=1
-                
-                
-    
-    for every body B in Bodies:
-
-        for everyother body B'' in Bodies:
-            integration(B, B'')
